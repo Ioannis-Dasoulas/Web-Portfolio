ad8c30d9ddd09ac1c83f93879e411ca461a85af4
u0154820 <ioannis.dasoulas@kuleuven.be>
2026-03-27 01:10:09 +0100

    add a few projects

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let pubButton = document.getElementById("pubButton");
     let proButton = document.getElementById("proButton");
     let pubDivs = document.querySelectorAll('.pubDiv');
     let proDivs = document.querySelectorAll('.proDiv');
-    
+
     // We add console.log to debug if they are found.
     console.log('pubButton:', pubButton);
     console.log('proButton:', proButton);
@@ -18,30 +18,30 @@ document.addEventListener("DOMContentLoaded", function() {
         console.error('Buttons not found!');
     }
 
-    function publicationsClick(e) { 
-        if(e) e.preventDefault();
+    function publicationsClick(e) {
+        if (e) e.preventDefault();
         console.log('Publications clicked');
         pubButton.classList.add("active");
         proButton.classList.remove("active");
 
-        pubDivs.forEach(function(div) {
+        pubDivs.forEach(function (div) {
             div.classList.remove("hideDiv");
         });
-        proDivs.forEach(function(div) {
+        proDivs.forEach(function (div) {
             div.classList.add("hideDiv");
         });
     }
 
-    function projectsClick(e) { 
-        if(e) e.preventDefault();
+    function projectsClick(e) {
+        if (e) e.preventDefault();
         console.log('Projects clicked');
         pubButton.classList.remove("active");
         proButton.classList.add("active");
 
-        pubDivs.forEach(function(div) {
+        pubDivs.forEach(function (div) {
             div.classList.add("hideDiv");
         });
-        proDivs.forEach(function(div) {
+        proDivs.forEach(function (div) {
             div.classList.remove("hideDiv");
         });
     }
